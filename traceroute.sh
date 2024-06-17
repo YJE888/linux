b8e908607d7a0d9364f9e698fe71a43b644f698b
YJE888 <75539276+YJE888@users.noreply.github.com>
2024-06-17 10:15:03 +0900

    Create traceroute.sh

---
+++ traceroute.sh
@@ -0,0 +1,39 @@
+## icmp를 사용하여 8.8.8.8 로의 경로 추적 후 일자별로 로그 쌓기
+#!/bin/bash
+
+today=`date +%y%m%d`
+TIME=`date +%r`
+FILE="/home/vps/icmp/${today}.txt"
+
+if [ ! -e ${FILE} ]; then
+    touch ${FILE}
+    echo >> ${FILE}
+    echo ${TIME} >> ${FILE}
+    sudo traceroute -I  8.8.8.8 >> ${FILE}
+else
+    echo >> ${FILE}
+    echo ${TIME} >> ${FILE}
+    sudo traceroute -I  8.8.8.8 >> ${FILE}
+fi
+
+exit 0
+---
+## udp를 사용하여 8.8.8.8로의 경로 추적 후 일자별로 로그 쌓기
+#!/bin/bash
+
+today=`date +%y%m%d`
+TIME=`date +%r`
+FILE="/home/vps/udp/${today}.txt"
+
+if [ ! -e ${FILE} ]; then
+    touch ${FILE}
+    echo >> ${FILE}
+    echo ${TIME} >> ${FILE}
+    sudo traceroute -w 1.0 -m 10  8.8.8.8 >> ${FILE}
+else
+    echo >> ${FILE}
+    echo ${TIME} >> ${FILE}
+    sudo traceroute -w 1.0 -m 10  8.8.8.8 >> ${FILE}
+fi
+
+exit 0
